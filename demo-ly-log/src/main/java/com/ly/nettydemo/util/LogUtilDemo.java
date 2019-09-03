7f717f06f87a244e1a9c0e6d842273f46ac68ee0
liuyang <378869781@qq.com>
2019-09-03 11:56:58 +0800

    新增一个在源代码里面打印日志的类

---
+++ demo-ly-log/src/main/java/com/ly/nettydemo/util/LogUtilDemo.java
@@ -0,0 +1,23 @@
+package com.ly.nettydemo.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ */
+public class LogUtilDemo {
+
+    public static void log(Object content) {
+        System.out.println(new SimpleDateFormat("MM-dd HH:mm:ss").format(new Date()) + " " +
+                           "[" + Thread.currentThread().getName() + "] " +
+                           content);
+    }
+    public static void log(Object content, Object that) {
+        System.out.println(
+                new SimpleDateFormat("MM-dd HH:mm:ss").format(new Date()) + " " +
+                "[" + Thread.currentThread().getName() + "] " +
+                that + " " +
+                content);
+    }
+}
